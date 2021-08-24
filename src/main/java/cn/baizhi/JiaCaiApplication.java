a1864578154352ab971f22653d1bb7f1b987bf8f
2101class <2171489535@qq.com>
2021-08-24 20:16:43 +0800

    Initial commit

---
+++ src/main/java/cn/baizhi/JiaCaiApplication.java
@@ -0,0 +1,13 @@
+package cn.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiaCaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiaCaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/baizhi/test/Testgit.java
@@ -0,0 +1,13 @@
+package cn.baizhi.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Testgit {
+    @Test
+    public void test1(){
+        System.out.println("cai");
+    }
+
+}
